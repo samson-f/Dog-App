9e5f57c03dd55f187796366ffee5b9b6202d43bc
samson-f <samsonfishman@gmail.com>
2025-02-16 22:33:33 +0200

    updated server.js

--- dogServer/server.js
+++ dogServer/server.js
@@ -7,6 +7,12 @@ const app = express();
 
 const uri = process.env.MONGO_URI;
 
+if (!uri) {
+    console.error('MONGO_URI not found in .env file');
+    process.exit(1); // Exit the process if MONGO_URI is not set
+}
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 mongoose.connect(uri, clientOptions);
 
